7d0f10fd6f613e8230cc945638fdc7299895aacc
Mirgo25 <mirgo2513@gmail.com>
2021-03-01 19:45:17 +0200

    Make admin, third commit.

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Author, Genre, Language, Book, BookInstance
 
-# Register your models here.
+# admin.site.register(Book)
+# admin.site.register(Author)
+
+admin.site.register(Genre)
+admin.site.register(Language)
+# admin.site.register(BookInstance)
+
+
+# Определяем класс для администрирования модели Author
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name',
+                    'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+
+# Регистрируем класс с соответствующей моделью Author
+admin.site.register(Author, AuthorAdmin)
+
+
+# Можно сделать проще через декоратор @register, который сразу зарегистрирует класс и модель
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    # display_genre - это ф-я для отображения жанра книги
+    # так как Django не позволяет напрямую поместить названия жанра из-за ManyToManyField
+    list_display = ('title', 'author', 'display_genre')
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
--- catalog/models.py
+++ catalog/models.py
@@ -36,6 +36,13 @@ class Book(models.Model):
         """
         return reverse('book_detail', args=[str(self.id)])
 
+    def display_genre(self):
+        """
+        Создаёт строку для жанра книги. Требуется для отображения жанра в Админке.
+        """
+        return ', '.join([genre.name for genre in self.genre.all()[:3]])
+    display_genre.short_description = 'Genre'
+
 
 class BookInstance(models.Model):
     """
@@ -80,7 +87,7 @@ class Author(models.Model):
         return reverse('author-detail', args=[str(self.id)])
 
     def __str__(self):
-        return f"{self.last_name} {self.first_name})"
+        return f"{self.last_name} {self.first_name}"
 
 
 class Language(models.Model):
